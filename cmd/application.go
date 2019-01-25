8692ba25d104a0a05d83c5403ecfd3edb35f8451
Massimo Vailati <massimo.vailati@rumbo.com>
2019-01-25 10:34:36 +0100

    [PLAT-1212] refactor

---
+++ cmd/application.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"git01.bravofly.com/n7/heimdall/cmd/client/colocation"
+	"git01.bravofly.com/n7/heimdall/cmd/client/waf"
+	"git01.bravofly.com/n7/heimdall/cmd/data_collector"
+	"git01.bravofly.com/n7/heimdall/cmd/metric"
+	"git01.bravofly.com/n7/heimdall/cmd/model"
+)
+
+func Orchestrator(config *model.Config) func() {
+	return func() {
+		aggregate := dataCollector(config)
+		metric.PushMetrics(aggregate, config)
+	}
+}
+
+func dataCollector(config *model.Config) []*model.Aggregate {
+
+	aggregate, _ := data_collector.GetZones()
+
+	aggregate, _ = data_collector.GetColocationTotals(aggregate, colocation.HttpColocations{
+		Config: config,
+	})
+	aggregate, _ = data_collector.GetWafTotals(aggregate, config, waf.HttpWafs{})
+
+	return aggregate
+}
--- cmd/run.go
+++ cmd/run.go
@@ -2,11 +2,7 @@ package cmd
 
 import (
 	"encoding/json"
-	"git01.bravofly.com/n7/heimdall/cmd/client/colocation"
-	"git01.bravofly.com/n7/heimdall/cmd/client/waf"
-	"git01.bravofly.com/n7/heimdall/cmd/data_collector"
 	"git01.bravofly.com/n7/heimdall/cmd/kubernetes"
-	"git01.bravofly.com/n7/heimdall/cmd/metric"
 	"git01.bravofly.com/n7/heimdall/cmd/model"
 	"git01.bravofly.com/n7/heimdall/cmd/scheduler"
 	"io/ioutil"
@@ -21,26 +17,11 @@ func Run() {
 	kubernetes.Readiness()
 
 	config := readConfig(os.Getenv("CONFIG_PATH"))
-	sched := scheduler.Scheduler{
+
+	scheduler.Scheduler{
 		Config: config,
-	}
+	}.Start(Orchestrator(config))
 
-	sched.Start(orchestrator(config))
-
-	//cronExpression := fmt.Sprintf("*/%s * * * *", sched.CollectEveryMinutes)
-	//
-	//logger.Printf("start collecting data at every %sth minute of the last %s minute ", config.CollectEveryMinutes, config.CollectEveryMinutes)
-	//
-	//c := cron.New()
-	//c.AddFunc(cronExpression, orchestrator(config))
-	//
-	//go c.Start()
-	//sig := make(chan os.Signal)
-	//signal.Notify(sig, os.Interrupt, os.Kill)
-	//s := <-sig
-	//c.Stop()
-	//
-	//fmt.Println("Got signal:", s)
 }
 
 func readConfig(filePath string) *model.Config {
@@ -54,23 +35,4 @@ func readConfig(filePath string) *model.Config {
 	var config *model.Config
 	json.Unmarshal([]byte(byteValue), &config)
 	return config
-}
-
-func orchestrator(config *model.Config) func() {
-	return func() {
-		aggregate := dataCollector(config)
-		metric.PushMetrics(aggregate, config)
-	}
-}
-
-func dataCollector(config *model.Config) []*model.Aggregate {
-
-	aggregate, _ := data_collector.GetZones()
-
-	aggregate, _ = data_collector.GetColocationTotals(aggregate, colocation.HttpColocations{
-		Config: config,
-	})
-	aggregate, _ = data_collector.GetWafTotals(aggregate, config, waf.HttpWafs{})
-
-	return aggregate
 }
