e829797b989032f302b6a1e61398d1ffc525a10e
Massimo Vailati <massimo.vailati@rumbo.com>
2019-01-29 16:36:20 +0100

    [PLAT-1212] added some tests

--- cmd/data_collector/zone_test.go
+++ cmd/data_collector/zone_test.go
@@ -11,5 +11,7 @@ func Test_zoneCollection(t *testing.T) {
 
 	assert.Equal(t, len(aggregates), 18)
 	assert.Equal(t, aggregates[0].ZoneName, "bravofly.at")
+	assert.Equal(t, aggregates[12].ZoneName, "rumbo.com")
 	assert.Equal(t, aggregates[0].ZoneID, "d746c5cf71899095e42c691788c3ccb9")
+	assert.Equal(t, aggregates[12].ZoneID, "5d3e4831360561bdfe9f498142f6d032")
 }
