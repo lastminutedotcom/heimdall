c0d968db2893b4dcae0b0d614fc5da24ee8a37cb
Massimo Vailati <massimo.vailati@rumbo.com>
2019-04-02 12:23:59 +0200

    increased time out on cloudflare client and descreased delay between calls

--- pkg/client/cloudflare.go
+++ pkg/client/cloudflare.go
@@ -18,7 +18,7 @@ var logger = log.New(os.Stdout, "[HEIMDALL] ", log.LstdFlags)
 var rateLimiter = rate.NewLimiter(rate.Limit(3), 1) // 3rps (900 req/5 min)
 
 var client = &http.Client{
-	Timeout: time.Duration(30 * time.Second),
+	Timeout: time.Duration(50 * time.Second),
 }
 
 func CloudflareClient() *cloudflare.API {
